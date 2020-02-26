ee9d19daa190f8129a70e0a0dde163b090fbb119
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-02-26 17:09:52 -0500

    added business

---
+++ Application/Activities/List.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        private readonly DataContext _context;
+        public List(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Activity>> getActivities(){
+            var activities = await _context.Activities.ToListAsync();
+            return activities;
+        }
+    }
+}
