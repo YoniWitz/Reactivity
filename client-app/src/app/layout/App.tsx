e5ad48baff94909d499f870d8eafb962caf0817b
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-02-28 10:53:41 -0500

    converted class to function

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,42 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header, Icon, List } from 'semantic-ui-react';
-import {IActivity} from './../models/acitivity';
+import { IActivity } from './../models/acitivity';
 
-interface IState {
-  activities : IActivity[]
-}
-class App extends Component<{}, IState> {
-  readonly state : IState= {
-    activities: []
-  }
+const App = () => {
+  let [activities, setActivities] = useState<IActivity[]>([]);
 
   //called immediately after the component is mounted
-  componentDidMount() {
+  useEffect(() => {
     fetch('http://localhost:5000/api/activities')
       .then(response => response.json())
-      .then((jsonResponse:IActivity[]) => {
-        this.setState({
-          activities: jsonResponse
-        })
-      })
-      .catch(err =>console.log(err, "error fetching activities data"));
-  }
+      .then((jsonResponse: IActivity[]) => setActivities(jsonResponse))
+      .catch(err => console.log(err, "error fetching activities data"))
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <Header as='h2'>
-          <Icon name='users' />
-          <Header.Content>Reacitivity</Header.Content>
-        </Header>
-        <List>
-          {this.state.activities.map((activitie) => (
-            <List.Item key={activitie.id}>{activitie.title}</List.Item>
-          ))
-          }
-        </List>
-      </div>)
-  }
+  return (
+    <div>
+      <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Reacitivity</Header.Content>
+      </Header>
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))
+        }
+      </List>
+    </div>)
 }
 
 export default App;
