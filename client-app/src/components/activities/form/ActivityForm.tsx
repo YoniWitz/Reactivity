85cc55706d89e01c5fc1c117dabfd6a944a02619
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-03-02 15:27:32 -0500

    fixed date and form

--- client-app/src/components/activities/form/ActivityForm.tsx
+++ client-app/src/components/activities/form/ActivityForm.tsx
@@ -50,7 +50,7 @@ export const ActivityForm: React.FC<IProps> = ({ onCancelForm, presentActivity, 
         <Form.Input placeholder="Title" value={activity.title} name="title" onChange={handleInputChange} />
         <Form.TextArea rows="2" placeholder="Description" value={activity.description} name="description" onChange={handleInputChange} />
         <Form.Input placeholder="Category" value={activity.category} name="category" onChange={handleInputChange} />
-        <Form.Input type="date" placeholder="Date" value={activity.date} name="date" onChange={handleInputChange} />
+        <Form.Input type="datetime-local" placeholder="Date" value={activity.date} name="date" onChange={handleInputChange} />
         <Form.Input placeholder="City" value={activity.city} name="city" onChange={handleInputChange} />
         <Form.Input placeholder="Venue" value={activity.venue} name="venue" onChange={handleInputChange} />
         <Button.Group widths="2">
