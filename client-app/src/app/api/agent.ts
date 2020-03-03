d1daf0503ff86b0a50d5119ab8b4b16a839eb804
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-03-03 09:26:48 -0500

    added persistence

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,23 +1,25 @@
 import axios, { AxiosResponse } from 'axios';
 import { IActivity } from '../models/acitivity';
 
-axios.defaults.baseURL = "http://localhost:5002/api";
+axios.defaults.baseURL = "http://localhost:5000/api";
 
 const responseBody = (response: AxiosResponse) => response.data;
 
 const requests = {
     get: (url: string) => axios.get(url).then(responseBody),
-    post: (url: string, body: IActivity) => axios.post(url, body).then(responseBody),
-    put: (url: string, body: IActivity) => axios.put(url, body).then(responseBody),
+    post: (url: string, body: IActivity): Promise<IActivity> => axios.post(url, body).then(responseBody),
+    put: (url: string, body: IActivity): Promise<IActivity> => axios.put(url, body).then(responseBody),
     delete: (url: string) => axios.delete(url).then(responseBody)
 }
 
-const activitiesUrl = '/activities';
+const activitiesUrl = '/activities'
 
 const Activities = {
-    list: () => requests.get(activitiesUrl),
-    details: (id: string) => requests.get(`${activitiesUrl}/${id}`),
-    update: (id: string, activity: IActivity) => requests.put(`${activitiesUrl}/${id}`, activity),
-    create: (activity: IActivity) => requests.post(activitiesUrl, activity),
-    delete: (id: string) => axios.delete('${activitiesUrl}/${id}')
+    list: (): Promise<IActivity[]> => requests.get(activitiesUrl),
+    details: (id: string): Promise<IActivity> => requests.get(`${activitiesUrl}/${id}`),
+    update: (id: string, activity: IActivity): Promise<IActivity> => requests.put(`${activitiesUrl}/${id}`, activity),
+    create: (activity: IActivity): Promise<IActivity> => requests.post(activitiesUrl, activity),
+    delete: (id: string) => axios.delete(`${activitiesUrl}/${id}`)
 }
+
+export default {Activities};
