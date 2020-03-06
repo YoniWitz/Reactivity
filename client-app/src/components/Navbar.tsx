8612b7e91775f7803a1436283050cc18af91b022
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-03-06 10:53:56 -0500

    added links to navbar

--- client-app/src/components/Navbar.tsx
+++ client-app/src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Menu, Container, Button, Modal } from "semantic-ui-react";
 import { ActivityForm } from "./activities/form/ActivityForm";
 import { IActivity } from "../app/models/acitivity";
+import { NavLink } from "react-router-dom";
 
 interface IProps {
   handleCreateSubmit: (activity: IActivity) => Promise<unknown>;
@@ -13,7 +14,7 @@ export const Navbar: React.FC<IProps> = ({ handleCreateSubmit, setSelectedActivi
   return (
     <Menu fixed='top' inverted>
       <Container>
-        <Menu.Item header>
+        <Menu.Item header as={NavLink} to='/' exact>
           <img
             src="./assets/logo.png"
             alt="logo"
@@ -21,8 +22,9 @@ export const Navbar: React.FC<IProps> = ({ handleCreateSubmit, setSelectedActivi
           />
           Reactivity
         </Menu.Item>
-        <Menu.Item name="Activities" />
+        <Menu.Item name="Activities" as={NavLink} to='/activities' />
         <Modal trigger={<Button color="green" size='medium' onClick={() => setModalOpen(true)}>Create Activity</Button>} open={modalOpen}>
+          <Modal.Header>Create New Activity</Modal.Header>
           <ActivityForm setSelectedActivity={setSelectedActivity} handleSubmit={handleCreateSubmit} onCancelForm={setModalOpen} presentActivity={null} />
         </Modal>
       </Container>
