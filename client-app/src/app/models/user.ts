043c9205203686fa468740a7990884b16bf3b54a
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-03-11 19:51:47 -0400

    added interfaces and axios apis

---
+++ client-app/src/app/models/user.ts
@@ -0,0 +1,18 @@
+export interface IUser{
+    displayname: string;
+    username: string;
+    token: string;
+}
+
+export interface ILoginUser{
+    email:string;
+    password:string;
+}
+
+export interface IRegisterUser{
+    email:string;  
+    displayname:string;
+    username:string;
+    password:string;
+}
+
