880af67d7d8e3bf7d71b8bebf7a10fee3f4acb64
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-03-16 15:58:51 -0400

    added api and validations

--- client-app/src/components/users/form/LoginForm.tsx
+++ client-app/src/components/users/form/LoginForm.tsx
@@ -2,16 +2,21 @@ import React, { useState, useEffect } from 'react'
 import { Form, Button, Message } from 'semantic-ui-react'
 import { ILoginUser } from '../../../app/models/IUser'
 import agent from '../../../app/api/agent'
+import { useHistory } from 'react-router-dom'
 
 export const LoginForm = () => {
     let [loginUser, setLoginUser] = useState<ILoginUser>({ email: '', password: '' })
     let [loading, setLoading] = useState<boolean>(false);
     let [submitDisabled, setSubmitDisabled] = useState<boolean>(true);
+    let [loggedIn, setLoggedin] = useState<boolean>(false);
+    let history = useHistory();
 
     useEffect(() => {
+        if(loggedIn) history.push('/activities');
+        
         let isEmailInvalid = loginUser.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i) ? false : true;
         setSubmitDisabled(loginUser.password.length < 6 || isEmailInvalid);
-    }, [loginUser]);
+    }, [loginUser, loggedIn]);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         let { name, value } = e.target;
@@ -21,7 +26,10 @@ export const LoginForm = () => {
         event.preventDefault();
         setLoading(true);
         agent.Users.login(loginUser)
-            .then(response => console.log(response))
+            .then(response => {
+                console.log(response);
+                setLoggedin(true);
+                })
             .catch(err => console.log(err))
             .finally(() => setLoading(false));
     }
@@ -30,7 +38,7 @@ export const LoginForm = () => {
             <Form.Input type="email" onChange={(e) => handleChange(e)} placeholder="Email" value={loginUser.email} name="email" />
             {loginUser.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i) ? null : <Message color='red'>Must enter valid email address </Message>}
             <Form.Input type="password" onChange={(e) => handleChange(e)} placeholder="Password" name="password" value={loginUser.password} />
-            {(submitDisabled) ? <Message color='red'>Password must contain  6 characters at least</Message> : null}
+            {(loginUser.password.length < 6) ? <Message color='red'>Password must contain  6 characters at least</Message> : null}
             <Button.Group widths="2">
                 <Button floated='right' positive type="submit" content="Login" disabled={submitDisabled} />
                 <Button floated='left' type="button" content="Clear Form" />
